55f9b335b236aee60d4f2c7edb8f846ec43bcbe5
omRubiks <om.rubiks@gmail.com>
2021-05-21 15:54:26 +0530

    Exp12

--- Exp 12/exp12.py
+++ Exp 12/exp12.py
@@ -1,22 +1,12 @@
 import pandas as pd
-data = {"Naruto":20,"Sasuke":23,"Sakura":17,"kakshi":27,"Zabuza":30,"Hashiram":47,"Madara":50}
-df = pd.Series(data)
 
-print(df[0])
-c1=0
-c2=0
-c3=0
-c4=0
-for value in df.items():
-    if df[value]<18:
-        c1+=1
-    elif df[value]>=18 and df[value]<25:
-        c2+=1
-    elif df[value]>=25 and df[value]<40:
-        c3+=1
-    elif df[value]>=40:
-        c4+=1
-    pass
+data = {'Name':['Naruto', 'Luffy', 'Zero2', 'Sayu','Kira','Hinata'],
+        'Age':[15, 21, 19, 18,28,24]}
+df = pd.DataFrame(data)
+print(df)
+print()
+age_group=[15,20,25,30,35]
+age_type=['Teen','Adult','Senior','Manager']
+df['age_Groups']=pd.cut(df['Age'],bins=age_group,labels=age_type,right=False)
 
-print("Below 18: "+str(c1))
-print (df)
+print(df)
